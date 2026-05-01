9a785f5eefcfd9c0be28a936ebd7b232fbede524
Beatriz Dias <157517503+beadias@users.noreply.github.com>
2026-05-01 19:44:26 -0300

    Adiciona .env ao gitignore

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const { Pool } = require('pg');
@@ -8,11 +10,11 @@ app.use(cors());
 app.use(express.json());
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'habit_tracker',
-    password: '763845',
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });
 
 app.get('/', (req, res) => {
